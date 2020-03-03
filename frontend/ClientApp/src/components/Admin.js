f88767dd795889a82d032e4918e550a060ef6ac8
piza9901 <johan.piza@users.noreply.github.com>
2020-03-03 17:25:51 -0500

    Se creo login y admin
    Se creo login y admin

--- frontend/ClientApp/src/components/Admin.js
+++ frontend/ClientApp/src/components/Admin.js
@@ -3,8 +3,94 @@ import React, { Component } from 'react'
 export default class Admin extends Component {
   render() {
     return (
-      <div>
-        Admin works!
+      <div className="container">
+
+
+        <div className="row mt-3 mt-md-5">
+
+          <div className="bg-white border rounded p-3 col-md-5">
+            <h3>Registro</h3>
+
+            <div class="form-group">
+              <label class="control-label">Nombre</label>
+              <input type="text" class="form-control" id="nombre_id" name="Nombre" />
+            </div>
+
+            <div class="form-group">
+              <label class="control-label">Edad</label>
+              <input type="text" class="form-control" id="edad_id" name="Edad" />
+            </div>
+
+            <div class="form-group">
+              <label for="nDocumnto_id" class="control-label">Tipo de documento</label>
+              <select class="form-control" id="nDocumento_id">
+                <option value="Masculino">Masculino</option>
+                <option value="Femenino">Femenino</option>
+                <option value="Otro">Otro</option>
+              </select>
+            </div>
+
+            <div class="form-group">
+              <label for="documento_id" class="control-label">Numero de documento</label>
+              <input type="text" class="form-control" id="documento_id" name="nDocumento" />
+            </div>
+
+            <div class="form-group">
+              <label for="tDocumento_id" class="control-label">Tipo de documento</label>
+              <select class="form-control" id="tDocumento_id">
+                <option value="Tarjeta de identidad">Tarjeta de identidad</option>
+                <option value="Cedula de ciudanania">Cedula de ciudanania</option>
+                <option value="Pasaporte">Pasaporte</option>
+              </select>
+            </div>
+
+            <div class="form-group">
+              <label for="correo_id" class="control-label">Correo</label>
+              <input type="text" class="form-control" id="correo_id" name="correo" />
+            </div>
+
+            <div class="form-group">
+              <label for="Clave_id" class="control-label">Clave</label>
+              <input type="text" class="form-control" id="zip_id" name="calve" />
+            </div>
+
+            <div class="form-group">
+              <button type="submit" class="btn btn-primary">Guardar</button>
+            </div>
+          </div>
+
+          <div class="col-sm-7">
+            <table class="table table-borderless">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">First</th>
+                  <th scope="col">Last</th>
+                  <th scope="col">Handle</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td colspan="2">Larry the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     )
   }
--- frontend/ClientApp/src/components/Login.js
+++ frontend/ClientApp/src/components/Login.js
@@ -3,8 +3,25 @@ import React, { Component } from 'react'
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        Login Works!
+      <div className="login-container">
+        <div className="bg-white border rounded p-4 col-md-6 offset-md-3 mt-3 mt-md-5">
+          <h3>Inicio de Sesión</h3>
+
+          <div>
+
+            <div className="form-group">
+              <label htmlFor="documento">Numero de documento:</label>
+              <input type="email" className="form-control" id="documento" aria-describedby="emailHelp" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="clave">Contraseña:</label>
+              <input type="password" className="form-control" id="clave" />
+            </div>
+            <button type="submit" className="btn btn-primary">Inciar sesión</button>
+
+          </div>
+
+        </div>
       </div>
     )
   }
